82272e8e23a6b2bd052bfea2ebfe61d1ba1734ca
Villa Golondrinas <admin@villagolondrinas.com>
2026-02-28 22:40:47 +0100

    Fix Vercel static generation

--- src/app/api/content/route.ts
+++ src/app/api/content/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
         let content = await prisma.siteContent.findUnique({
